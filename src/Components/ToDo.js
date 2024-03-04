9d371c66b891067d95dc7f6e04e2845061d3bccd
Ajay Joy <ajayjoy618@gmail.com>
2024-03-04 14:16:03 +0530

    added button ,it's css

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState,useRef,useEffect } from 'react';
 import './ToDo.css';
+import { IoMdDoneAll } from "react-icons/io";
+import { FiEdit } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
 
 function ToDo() {
     const [todo, setTodo] = useState('');
@@ -17,23 +20,44 @@ function ToDo() {
         }
     };
 
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            addToDo();
+        }
+    };
+
+    const inputRef = useRef ('null')
+
+    useEffect(()=>{
+        inputRef.current.focus();
+    })
+
     return (
         <div className='container'>
             <h2>TODO APP</h2>
             <form className='form-group' onSubmit={handleSubmit}>
                 <input
                     type="text"
+                    ref={inputRef}
                     value={todo}
                     placeholder='Enter your Todo'
                     className='form-control'
                     onChange={(event) => setTodo(event.target.value)}
+                    onKeyPress={handleKeyPress}
                 />
                 <button type="button" onClick={addToDo}>ADD</button>
             </form>
             <div className='list'>
                 <ul>
                     {todos.map((todo, index) => (
-                        <li key={index}>{todo}</li>
+                        <li className='list-items' key={index}>
+                            <div className='list-item-list'> {todo}</div>  
+                        <span>
+                            <IoMdDoneAll className='list-item-icons' id='complete' title='Complete'/>
+                            <FiEdit className='list-item-icons' id='edit' title='Edit'/>
+                            <MdDelete className='list-item-icons' id='delete' title='Delete'/>
+                        </span>
+                        </li>
                     ))}
                 </ul>
             </div>
