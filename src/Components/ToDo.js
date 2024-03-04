c9250711d53c3ba9eaddfc5b04dfa36ef3c5db47
Ajay Joy <ajayjoy618@gmail.com>
2024-03-04 13:07:13 +0530

    state for todo and todo add

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,23 +1,44 @@
-import React from 'react'
-import './ToDo.css'
+import React, { useState } from 'react';
+import './ToDo.css';
 
 function ToDo() {
-  return (
-    <div className='container'>
-      <h2> TODO APP</h2>
-      <form className='form-group'>
-        <input type="text" placeholder='Enter your Todo' className='form-control'/>
-        <button>ADD</button>
-      </form>
-      <div className='list'>
-        <ul>
-            <li  className='form-control'>First</li>
-            <li  className='form-control'>Second</li>
-            <li  className='form-control'>Third</li>
-        </ul>
-      </div>
-    </div>
-  )
+    const [todo, setTodo] = useState('');
+    const [todos, setTodos] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const addToDo = () => {
+        if (todo.trim() !== '') {
+            setTodos([...todos, todo]);
+            console.log(todos);
+            setTodo('');
+        }
+    };
+
+    return (
+        <div className='container'>
+            <h2>TODO APP</h2>
+            <form className='form-group' onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={todo}
+                    placeholder='Enter your Todo'
+                    className='form-control'
+                    onChange={(event) => setTodo(event.target.value)}
+                />
+                <button type="button" onClick={addToDo}>ADD</button>
+            </form>
+            <div className='list'>
+                <ul>
+                    {todos.map((todo, index) => (
+                        <li key={index}>{todo}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
-export default ToDo
+export default ToDo;
